b081781d1075b3afe0b424cc0264b55353380494
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-09-24 02:38:39 +0900

    react react  and initial setup
    초기설정
    1.react-router-dom 설치
    2.styled-reset 설치
    3.styled-components 설치
    4.폰트 설정: robot

---
+++ ddd_front/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./style";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
+
+export default App;
